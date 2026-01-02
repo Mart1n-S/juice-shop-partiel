cf6f7030f30d36aee02f00a7e00ef264e3673338
Martin <martinsimongo@gmail.com>
2026-01-02 16:17:38 +0100

    fix(nosql-injection): sanitize inputs in updateProductReviews

--- routes/updateProductReviews.ts
+++ routes/updateProductReviews.ts
@@ -10,22 +10,41 @@ import { challenges } from '../data/datacache'
 import * as security from '../lib/insecurity'
 import * as db from '../data/mongodb'
 
-// vuln-code-snippet start noSqlReviewsChallenge forgedReviewChallenge
 export function updateProductReviews () {
   return (req: Request, res: Response, next: NextFunction) => {
-    const user = security.authenticatedUsers.from(req) // vuln-code-snippet vuln-line forgedReviewChallenge
-    db.reviewsCollection.update( // vuln-code-snippet neutral-line forgedReviewChallenge
-      { _id: req.body.id }, // vuln-code-snippet vuln-line noSqlReviewsChallenge forgedReviewChallenge
-      { $set: { message: req.body.message } },
-      { multi: true } // vuln-code-snippet vuln-line noSqlReviewsChallenge
+    const user = security.authenticatedUsers.from(req)
+
+    const reviewId = String(req.body.id)
+    const message = String(req.body.message)
+
+    db.reviewsCollection.update(
+      { _id: reviewId },
+      { $set: { message } },
+      { multi: false }
     ).then(
       (result: { modified: number, original: Array<{ author: any }> }) => {
-        challengeUtils.solveIf(challenges.noSqlReviewsChallenge, () => { return result.modified > 1 }) // vuln-code-snippet hide-line
-        challengeUtils.solveIf(challenges.forgedReviewChallenge, () => { return user?.data && result.original[0] && result.original[0].author !== user.data.email && result.modified === 1 }) // vuln-code-snippet hide-line
+        challengeUtils.solveIf(
+          challenges.noSqlReviewsChallenge,
+          () => { return result.modified > 1 }
+        )
+
+        challengeUtils.solveIf(
+          challenges.forgedReviewChallenge,
+          () => {
+            return (
+              user?.data &&
+              result.original[0] &&
+              result.original[0].author !== user.data.email &&
+              result.modified === 1
+            )
+          }
+        )
+
         res.json(result)
-      }, (err: unknown) => {
+      },
+      (err: unknown) => {
         res.status(500).json(err)
-      })
+      }
+    )
   }
 }
-// vuln-code-snippet end noSqlReviewsChallenge forgedReviewChallenge
