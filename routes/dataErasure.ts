77888c2353e20a8446ab911ec8246fd2c4ede411
Martin <martinsimongo@gmail.com>
2026-01-04 14:05:29 +0100

    fix(security): remove hardcoded wallet mnemonic and load from environment

--- routes/dataErasure.ts
+++ routes/dataErasure.ts
@@ -1,101 +1,71 @@
-/*
- * Copyright (c) 2014-2025 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
-import express, { type NextFunction, type Request, type Response } from 'express'
-
-import { SecurityQuestionModel } from '../models/securityQuestion'
-import { PrivacyRequestModel } from '../models/privacyRequests'
-import { SecurityAnswerModel } from '../models/securityAnswer'
+import express, { type Request, type Response } from 'express'
+import { HDNodeWallet } from 'ethers'
 import * as challengeUtils from '../lib/challengeUtils'
+import * as utils from '../lib/utils'
 import { challenges } from '../data/datacache'
-import * as security from '../lib/insecurity'
-import { UserModel } from '../models/user'
-
 const router = express.Router()
 
-router.get('/', async (req: Request, res: Response, next: NextFunction) => {
-  const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
-  if (!loggedInUser) {
-    next(new Error('Blocked illegal activity by ' + req.socket.remoteAddress))
-    return
-  }
-
-  const email = loggedInUser.data.email
-
-  try {
-    const answer = await SecurityAnswerModel.findOne({
-      include: [{
-        model: UserModel,
-        where: { email }
-      }]
-    })
-
-    if (answer == null) {
-      throw new Error('No answer found!')
-    }
-
-    const question = await SecurityQuestionModel.findByPk(answer.SecurityQuestionId)
-    if (question == null) {
-      throw new Error('No question found!')
-    }
-
-    res.render('dataErasureForm', {
-      userEmail: email,
-      securityQuestion: question.question
-    })
-  } catch (error) {
-    next(error)
-  }
-})
-
-interface DataErasureRequestParams {
-  email: string
-  securityAnswer: string
-}
-
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-router.post(
-  '/',
-  async (
-    req: Request<Record<string, unknown>, Record<string, unknown>, DataErasureRequestParams>,
-    res: Response,
-    next: NextFunction
-  ): Promise<void> => {
-    const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
-    if (!loggedInUser) {
-      next(new Error('Blocked illegal activity by ' + req.socket.remoteAddress))
-      return
-    }
-
+export function checkKeys () {
+  return (req: Request, res: Response) => {
     try {
-      await PrivacyRequestModel.create({
-        UserId: loggedInUser.data.id,
-        deletionRequested: true
+      const mnemonic = process.env.WALLET_MNEMONIC
+
+      if (!mnemonic) {
+        return res.status(500).json({
+          success: false,
+          message: 'Wallet mnemonic is not configured'
+        })
+      }
+
+      const mnemonicWallet = HDNodeWallet.fromPhrase(mnemonic)
+      const privateKey = mnemonicWallet.privateKey
+      const publicKey = mnemonicWallet.publicKey
+      const address = mnemonicWallet.address
+
+      challengeUtils.solveIf(challenges.nftUnlockChallenge, () => {
+        return req.body.privateKey === privateKey
       })
 
-      res.clearCookie('token')
-
-      // Toujours utiliser une vue fixe (pas de layout contrôlé par l'utilisateur)
-      res.render(
-        'dataErasureResult',
-        {
-          ...req.body
-        },
-        (error, html) => {
-          if (!html || error) {
-            next(new Error(error?.message))
-          } else {
-            const sendlfrResponse: string = html.slice(0, 100) + '......'
-            res.send(sendlfrResponse)
-            challengeUtils.solveIf(challenges.lfrChallenge, () => true)
-          }
-        }
-      )
+      if (req.body.privateKey === privateKey) {
+        res.status(200).json({
+          success: true,
+          message: 'Challenge successfully solved',
+          status: challenges.nftUnlockChallenge
+        })
+      } else if (req.body.privateKey === address) {
+        res.status(401).json({
+          success: false,
+          message: 'Looks like you entered the public address of my ethereum wallet!',
+          status: challenges.nftUnlockChallenge
+        })
+      } else if (req.body.privateKey === publicKey) {
+        res.status(401).json({
+          success: false,
+          message: 'Looks like you entered the public key of my ethereum wallet!',
+          status: challenges.nftUnlockChallenge
+        })
+      } else {
+        res.status(401).json({
+          success: false,
+          message: 'Looks like you entered a non-Ethereum private key to access me.',
+          status: challenges.nftUnlockChallenge
+        })
+      }
     } catch (error) {
-      next(error)
+      res.status(500).json(utils.getErrorMessage(error))
     }
   }
-)
+}
 
+export function nftUnlocked () {
+  return (req: Request, res: Response) => {
+    try {
+      res.status(200).json({
+        status: challenges.nftUnlockChallenge.solved
+      })
+    } catch (error) {
+      res.status(500).json(utils.getErrorMessage(error))
+    }
+  }
+}
 export default router
